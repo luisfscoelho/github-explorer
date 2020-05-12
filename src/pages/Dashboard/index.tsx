e4b509727e286c8d5d17e445b7855e5c49f3a19e
Luis Coelho <luis.fs.coelho@pm.me>
2020-05-12 20:41:09 -0300

    Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,62 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import { Title, Form, Repositories } from './styles';
+
+import logoIMG from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoIMG} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/22839492?s=460"
+            alt="Luis Coelho"
+          />
+          <div>
+            <strong>luiscoelho/myrepo</strong>
+            <p>A project to do any thing!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/22839492?s=460"
+            alt="Luis Coelho"
+          />
+          <div>
+            <strong>luiscoelho/myrepo</strong>
+            <p>A project to do any thing!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/22839492?s=460"
+            alt="Luis Coelho"
+          />
+          <div>
+            <strong>luiscoelho/myrepo</strong>
+            <p>A project to do any thing!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
